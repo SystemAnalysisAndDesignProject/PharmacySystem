4d0a0c3cb60b01c9277e8014f3a1d3630f270141
MadeWithPassion <wrzosek71@gmail.com>
2016-10-25 01:48:19 +0100

    Modification

--- src/com/psmodel/PharmacyConstants.java
+++ src/com/psmodel/PharmacyConstants.java
@@ -3,4 +3,5 @@ package com.psmodel;
 public class PharmacyConstants {
     
     public static final String usersFilePath = "C:\\Users\\paul7_000\\Documents\\NetBeansProjects\\PharmacySystem\\res\\Users.txt";
+    public static final int ss = 1;
 }
